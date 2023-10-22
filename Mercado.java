22a8ed02e3678b43e1f901a63f802d6af610fe42
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2023-10-21 23:04:50 -0600

    Add files via upload

--- Mercado.java
+++ Mercado.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
 
 public class Mercado {
     public static void main(String[] args) {
         SistemaInventario sistema = new SistemaInventario();
-        sistema.cargarProductosDesdeCSV("inventario.csv"); // Carga el archivo "inventario.csv" automaticamente
+        sistema.cargarProductosDesdeCSV("inventario.csv"); // Carga el archivo "inventario.csv" automa6ticamente
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -21,8 +22,64 @@ public class Mercado {
 
             switch (opcion) {
                 case 1:
-                    // La opcion 1 ya carga automaticamente el archivo "inventario.csv"
-                    System.out.println("Productos cargados exitosamente.");
+                    System.out.print("Ingrese el ID del nuevo producto: ");
+                    int nuevoId = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Ingrese el nombre del nuevo producto: ");
+                    String nuevoNombre = scanner.nextLine();
+                    System.out.print("Ingrese la cantidad disponible: ");
+                    int nuevaCantidadDisponible = scanner.nextInt();
+                    scanner.nextLine(); // Consumir la linea en blanco
+
+                    System.out.print("Ingrese la cantidad vendida: ");
+                    int nuevaCantidadVendidos = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Ingrese el estado (disponible o no disponible): ");
+                    String nuevoEstado = scanner.nextLine();
+
+                    System.out.print("Ingrese el precio: ");
+                    double nuevoPrecio = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.print("Ingrese la categoria (Bebida, Snack o Cereales): ");
+                    String nuevaCategoria = scanner.nextLine();
+
+                    Producto nuevoProducto = null;
+
+                    if (nuevaCategoria.equals("Bebida")) {
+                        System.out.print("Ingrese los mililitros (ml): ");
+                        int nuevoMl = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Ingrese el tipo (energetica, con licor, natural, agua pura): ");
+                        String nuevoTipo = scanner.nextLine();
+                        nuevoProducto = new Bebida(nuevoId, nuevoNombre, nuevaCantidadDisponible, nuevaCantidadVendidos, nuevoEstado, nuevoPrecio, nuevoMl, nuevoTipo);
+                    } else if (nuevaCategoria.equals("Snack")) {
+                        System.out.print("Ingrese los gramos (gr): ");
+                        int nuevoGramos = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Ingrese el sabor (barbacoa, picante, natural, etc.): ");
+                        String nuevoSabor = scanner.nextLine();
+                        System.out.print("Ingrese el tamaño (individual, familiar, pequeño, etc.): ");
+                        String nuevoTamaño = scanner.nextLine();
+                        nuevoProducto = new Snack(nuevoId, nuevoNombre, nuevaCantidadDisponible, nuevaCantidadVendidos, nuevoEstado, nuevoPrecio, nuevoGramos, nuevoSabor, nuevoTamaño);
+                    } else if (nuevaCategoria.equals("Cereales")) {
+                        System.out.print("Ingrese los gramos (gr): ");
+                        int nuevoGramos = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Ingrese el sabor (barbacoa, picante, natural, etc.): ");
+                        String nuevoSabor = scanner.nextLine();
+                        System.out.print("Ingrese el tamaño (individual, familiar, pequeño, etc.): ");
+                        String nuevoTamaño = scanner.nextLine();
+                        nuevoProducto = new Cereales(nuevoId, nuevoNombre, nuevaCantidadDisponible, nuevaCantidadVendidos, nuevoEstado, nuevoPrecio, nuevoGramos, nuevoSabor, nuevoTamaño);
+                    } else {
+                        System.out.println("Categoria no valida. No se pudo crear el producto.");
+                    }
+
+                    if (nuevoProducto != null) {
+                        sistema.agregarProducto(nuevoProducto);
+                        System.out.println("Nuevo producto agregado exitosamente.");
+                    }
                     break;
                 case 2:
                     System.out.print("Ingrese el ID del producto a buscar: ");
@@ -41,7 +98,7 @@ public class Mercado {
                     sistema.listarProductosPorCategoria(categoria).forEach(producto -> System.out.println(producto.getNombre()));
                     break;
                 case 4:
-                    System.out.print("Ingrese la categoria para calcular la comision: ");
+                    System.out.print("Ingrese la categoría para calcular la comisión: ");
                     String categoriaComision = scanner.nextLine();
                     sistema.calcularComisionPorCategoria(categoriaComision);
                     System.out.println("Comisión calculada para la categoria: " + categoriaComision);
--- SistemaInventario.java
+++ SistemaInventario.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,66 +17,110 @@ public class SistemaInventario {
         inventario = cargarCSV(archivoCSV);
     }
 
-private List<Producto> cargarCSV(String archivoCSV) {
-    List<Producto> productos = new ArrayList<>();
-    String linea;
+    private List<Producto> cargarCSV(String archivoCSV) {
+        List<Producto> productos = new ArrayList<>();
+        String linea;
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(archivoCSV))) {
-        // Omitimos la primera linea que generalmente contiene encabezados
-        reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivoCSV))) {
+            // Omitimos la primera línea que generalmente contiene encabezados
+            reader.readLine();
 
-        while ((linea = reader.readLine()) != null) {
-            String[] campos = linea.split(",");
-            if (campos.length >= 7) {
-                int id = Integer.parseInt(campos[0]);
-                String nombre = campos[1];
-                int cantidadDisponible = Integer.parseInt(campos[2]);
-                int cantidadVendidos = Integer.parseInt(campos[3]);
-                String estado = campos[4];
-                double precio = Double.parseDouble(campos[5]);
-                String categoria = campos[6];
-                Producto producto = null;
+            while ((linea = reader.readLine()) != null) {
+                String[] campos = linea.split(",");
+                if (campos.length >= 7) {
+                    int id = Integer.parseInt(campos[0]);
+                    String nombre = campos[1];
+                    int cantidadDisponible = Integer.parseInt(campos[2]);
+                    int cantidadVendidos = Integer.parseInt(campos[3]);
+                    String estado = campos[4];
+                    double precio = Double.parseDouble(campos[5]);
+                    String categoria = campos[6];
+                    Producto producto = null;
 
-                if (categoria.equals("Bebida")) {
-                    if (campos.length >= 8) {
-                        int ml = Integer.parseInt(campos[7]);
-                        if (campos.length >= 9) {
-                            String tipo = campos[8];
-                            producto = new Bebida(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, ml, tipo);
-                        } else {
-                            producto = new Bebida(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, ml, "");
-                        }
-                    }
-                } else if (categoria.equals("Snack") || categoria.equals("Cereales")) {
-                    if (campos.length >= 10) {
-                        int gramos = Integer.parseInt(campos[9]);
-                        if (campos.length >= 11) {
-                            String sabor = campos[10];
-                            if (campos.length >= 12) {
-                                String tamaño = campos[11];
-                                producto = (categoria.equals("Snack")) ? new Snack(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, tamaño)
-                                                                     : new Cereales(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, tamaño);
+                    if (categoria.equals("Bebida")) {
+                        if (campos.length >= 8) {
+                            int ml = Integer.parseInt(campos[7]);
+                            if (campos.length >= 9) {
+                                String tipo = campos[8];
+                                producto = new Bebida(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, ml, tipo);
                             } else {
-                                producto = (categoria.equals("Snack")) ? new Snack(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, "")
-                                                                     : new Cereales(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, "");
+                                producto = new Bebida(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, ml, "");
+                            }
+                        }
+                    } else if (categoria.equals("Snack") || categoria.equals("Cereales")) {
+                        if (campos.length >= 10) {
+                            int gramos = Integer.parseInt(campos[9]);
+                            if (campos.length >= 11) {
+                                String sabor = campos[10];
+                                if (campos.length >= 12) {
+                                    String tamaño = campos[11];
+                                    producto = (categoria.equals("Snack")) ? new Snack(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, tamaño)
+                                                                         : new Cereales(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, tamaño);
+                                } else {
+                                    producto = (categoria.equals("Snack")) ? new Snack(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, "")
+                                                                         : new Cereales(id, nombre, cantidadDisponible, cantidadVendidos, estado, precio, gramos, sabor, "");
+                                }
                             }
                         }
                     }
-                }
 
-                if (producto != null) {
-                    productos.add(producto);
+                    if (producto != null) {
+                        productos.add(producto);
+                    }
                 }
             }
+            System.out.println("Productos cargados exitosamente desde el archivo CSV.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al cargar productos desde el archivo CSV.");
         }
-        System.out.println("Productos cargados exitosamente desde el archivo CSV.");
-    } catch (IOException e) {
-        e.printStackTrace();
-        System.out.println("Error al cargar productos desde el archivo CSV.");
+
+        return productos;
     }
 
-    return productos;
-}
+    public void agregarProducto(Producto producto) {
+        inventario.add(producto);
+        // También guarda los cambios en el archivo CSV
+        guardarCSV("inventario.csv", inventario);
+    }
+
+    private void guardarCSV(String archivoCSV, List<Producto> productos) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivoCSV))) {
+            // Escribir el encabezado del archivo CSV
+            writer.println("id_producto,nombre,cantidad_disponible,cantidad_vendidos,estado,precio,categoria,ml,tipo,gramos,sabor,tamaño");
+
+            // Escribir los datos de los productos
+            for (Producto producto : productos) {
+                writer.print(producto.getId() + ",");
+                writer.print(producto.getNombre() + ",");
+                writer.print(producto.getCantidadDisponible() + ",");
+                writer.print(producto.getCantidadVendidos() + ",");
+                writer.print(producto.getEstado() + ",");
+                writer.print(producto.getPrecio() + ",");
+                writer.print(producto.getCategoria() + ",");
+
+                if (producto instanceof Bebida) {
+                    Bebida bebida = (Bebida) producto;
+                    writer.print(bebida.getMl() + ",");
+                    writer.print(bebida.getTipo() + ",");
+                    writer.print("0,0,"); // Campos no utilizados para Bebidas
+                } else if (producto instanceof Snack) {
+                    Snack snack = (Snack) producto;
+                    writer.print("0,0,"); // Campos no utilizados para Bebidas
+                    writer.print(snack.getGramos() + ",");
+                    writer.print(snack.getSabor() + ",");
+                    writer.print(snack.getTamaño() + ",");
+                }
+
+                writer.println(); // Nueva línea para el siguiente producto
+            }
+
+            System.out.println("Productos guardados exitosamente en el archivo CSV.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al guardar productos en el archivo CSV.");
+        }
+    }
 
     public Producto buscarProductoPorID(int id) {
         for (Producto producto : inventario) {
